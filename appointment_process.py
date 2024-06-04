f42e61e6b554a39842c39e250084f2a72fdade60
Prabhat <prabhatranjan2206@gmail.com>
2024-06-04 22:29:59 +0530

    added screeshot and reporting tool

--- appointment_process.py
+++ appointment_process.py
@@ -1,8 +1,6 @@
 from commonLib import Library
 from main_setup import *
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 
 class Appointment_Process():
 
@@ -17,7 +15,7 @@ class Appointment_Process():
         self.facility_message = "Facility"
         self.health_program_message = "Healthcare Program"
         self.vist_date_message = "Visit Date (Required)"
-        self.comment_message = "Comment"
+        self.comment_message = "Comments"
 
     def verify_appointment_page(self):
         try:
@@ -26,6 +24,7 @@ class Appointment_Process():
             appointment_header_text = appointment_heading.text
             assert appointment_header_text == self.appointment_header_message, f"Error in validation message, Expected Message: {self.appointment_header_message}, but got Actual Message: {appointment_header_text} "
         except Exception as e:
+            self.driver.get_screenshot_as_file('appointment_heading.png')
             print(f"Verify Appointment Page Error: {e} ")
             raise
 
@@ -40,5 +39,6 @@ class Appointment_Process():
             assert vist_date_element.text == self.vist_date_message, f"Actual Value: {vist_date_element.text} does not match with Expected Value: {self.vist_date_message}"
             assert comment_element.text == self.comment_message, f"Actual Value: {comment_element.text} does not match with Expected Value: {self.comment_message}"
         except Exception as e:
+            self.driver.get_screenshot_as_file('webpage_element.png')
             print(f"Appointment Page element Error: {e} ")
             raise
